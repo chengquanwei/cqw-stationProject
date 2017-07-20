2eb763659ddcf37e1b7a3952c907659c17cae00e
chengquanwei <1170001884@qq.com>
2017-07-20 21:32:14 +0800

    增加layer弹出层

--- src/main/webapp/js/pagesJs/userList.js
+++ src/main/webapp/js/pagesJs/userList.js
@@ -7,6 +7,10 @@ $(function(){
 		success:function(result){
 			console.log(result);
 			if(result.meta.message == "ok"){
+				layer.tips('framework测试成功！数据已从数据库中取出！', '', {
+					  tips: [1, '#3595CC'],
+					  time: 1000
+				});
 				var res = result.data;
 				var info = "";
 				for(var i = 0;i<res.length;i++){
